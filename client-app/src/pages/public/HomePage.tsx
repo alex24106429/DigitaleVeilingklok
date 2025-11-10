b423370e598ca72bc5d6fc9cbfe0795488c232b6
alex24106429 <188322175+alex24106429@users.noreply.github.com>
2025-11-10 09:33:44 +0100

    Improve HomePage

--- client-app/src/pages/public/HomePage.tsx
+++ client-app/src/pages/public/HomePage.tsx
@@ -3,14 +3,22 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { useAuth } from '../../contexts/AuthContext';
 import { NavLink } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 export default function HomePage() {
+	const features = [
+		'Toegang tot grote markt',
+		'Gemakkelijk zaken doen',
+		'Handige extra diensten',
+	];
 	const { user } = useAuth();
 	return (
 		<div>
 			<Box bgcolor={"primary.100"} width="100vw" padding={"50px"}>
-				<Typography variant="h2" component="h1" gutterBottom color={"secondary.700"}>
-					Welkom bij PetalBid
+				<Typography variant="h3" component="h1" gutterBottom color={"secondary.700"}>
+					Planten ver/inkopen vanuit één plek
 				</Typography>
 				<Typography mb="20px">
 					Handel al je commerciële processen eenvoudig af op één plek. PetalBid, het wereldwijde platform voor de sierteeltsector!
@@ -24,6 +32,48 @@ export default function HomePage() {
 					Meer info
 				</Button>
 			</Box>
+			<Grid container spacing={4} alignItems="center">
+				<Grid size={{ xs: 12, md: 6 }}>
+					<Box padding={{ xs: 2, md: 4 }}>
+						<Typography variant="h4" component="h2" gutterBottom color={"secondary.700"}>
+							Wat is PetalBid?
+						</Typography>
+						<Typography mb="10px">
+							PetalBid is hét digitale platform voor de sierteelt. Met PetalBid bieden we één centrale plek aan waar kwekers en kopers hun commerciële en logistieke processen eenvoudig kunnen afhandelen. Daarnaast biedt PetalBid functionaliteiten voor dienstverleners (zoals handelsagenten en transporteurs) en biedt het koppelingen (API) voor softwareleveranciers.
+						</Typography>
+						{features.map((feature) => (
+							<FormControlLabel
+								key={feature}
+								control={<Checkbox disabled checked sx={{ '&.Mui-disabled': { color: "primary.600" } }} />}
+								label={feature}
+								sx={{
+									width: '100%',
+									'& .MuiFormControlLabel-label.Mui-disabled': {
+										fontWeight: "bold",
+										color: "secondary.700",
+									}
+								}}
+							/>
+						))}
+					</Box>
+				</Grid>
+
+				<Grid size={{ xs: 12, md: 6 }}>
+					<Box
+						component="img"
+						src="/images/stock-1.avif"
+						alt="Mensen die de hand schudden en glimlachen. Op de achtergrond zijn kleurrijke bloemen en planten te zien, de foto is genomen tijdens een evenement."
+						sx={{
+							borderRadius: "20px",
+							margin: '20px',
+							width: 'calc(100% - 40px)',
+							float: 'right',
+							height: 'auto',
+							display: 'block'
+						}}
+					/>
+				</Grid>
+			</Grid>
 		</div>
 	);
 }
